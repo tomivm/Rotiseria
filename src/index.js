bcda658d9566f9b63ad4a6d6ab62e36b75c880fe
tomivm <tomivm98@gmail.com>
2021-02-02 04:07:57 +0100

    primeras tareas.01

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-/* Aca voy a comentar mi usuario de github y voy a hacer un push a la rama master.
+/* Aca voy a comentar mi usuario de github y voy a hacer un push a la rama *main.
  Nos ubicamos en el directorio principal /Rotiseria.
  En este repo colabora:
 -@patagonikus
