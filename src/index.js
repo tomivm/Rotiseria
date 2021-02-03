15a619e08102ba95489d00bc828504534d752d8b
achaga <achagafabrizio@gmail.com>
2021-02-03 19:19:50 -0300

    corrijo anterior, y queda piola de nuevo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-/* Aca voy a comentar mi usuario de github y voy a hacer un push a la rama *main.
- Nos ubicamos en el directorio principal /Rotiseria.
- En este repo colabora:
--@patagonikus
--@tomivm 
--@joseracosky
--@achaga
-// */
-
-// Probando
+//  Aca voy a comentar mi usuario de github y voy a hacer un push a la rama *main.
+//  Nos ubicamos en el directorio principal /Rotiseria.
+//  En este repo colabora:
+// -@patagonikus
+// -@tomivm 
+// -@joseracosky
+// -@achaga
